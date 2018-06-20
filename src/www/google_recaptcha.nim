53b6464ccfc375d9b4ce9ec862b98d58b793bfa6
ThomasTJdev <ttjarloev@gmail.com>
2018-06-20 22:46:05 +0200

    Recaptcha module init

---
+++ src/www/google_recaptcha.nim
@@ -0,0 +1,44 @@
+# Copyright 2018 - Thomas T. Jarløv
+
+import recaptcha, parsecfg, asyncdispatch, os
+
+
+var
+  useCaptcha*: bool
+  captcha*: ReCaptcha
+  
+
+# Using config.ini
+let dict = loadConfig("config/config.cfg")
+
+# Web settings
+let recaptchaSecretKey = dict.getSectionValue("reCAPTCHA","Secretkey")
+let recaptchaSiteKey* = dict.getSectionValue("reCAPTCHA","Sitekey")
+
+
+proc setupReCapthca*() =
+  # Activate Google reCAPTCHA
+  if len(recaptchaSecretKey) > 0 and len(recaptchaSiteKey) > 0:
+    useCaptcha = true
+    captcha = initReCaptcha(recaptchaSecretKey, recaptchaSiteKey)
+
+  else:
+    useCaptcha = false
+
+
+proc checkReCaptcha*(antibot, userIP: string): Future[bool] {.async.} =
+  if useCaptcha:
+    var captchaValid: bool = false
+    try:
+      captchaValid = await captcha.verify(antibot, userIP)
+    except:
+      captchaValid = false
+
+    if not captchaValid:
+      return false
+      
+    else:
+      return true
+  
+  else:
+    return true
