79746d01ba2f3efaa1433c822428138e3e0dd853
Federico Ceratto <federico.ceratto@gmail.com>
2019-03-30 18:17:17 +0000

    Prepare for system-wide installation
    Handle system-wide paths
    Setup Nimble in dedicated dir and install build deps
    Code cleanup

--- nimhapkg/resources/utils/common.nim
+++ nimhapkg/resources/utils/common.nim
@@ -1,6 +1,6 @@
 
 import parsecfg
-from os import getAppDir
+from os import getAppDir, `/`
 from strutils import replace
 
 proc loadConf*(modulename: string): Config =
@@ -17,4 +17,26 @@ proc loadConf*(modulename: string): Config =
 
   echo("Reading cfg file " & fn)
   loadConfig(fn)
+
+#installpath
+const systmp = "/var/run/nimha/tmp"
+
+proc getTmpDir*(modulename: string): string =
+  ## Temporary directory, not persistent across restarts
+  when defined(dev):
+    replace(getAppDir(), "/nimhapkg/mainmodules", "") / "/tmp"
+  else:
+    if modulename == "":
+      systmp / "nimha"
+    else:
+      # TODO: check for path traversal?
+      systmp / modulename
+
+proc getNimbleCache*(): string =
+  ## Get Nimble cache
+  when defined(dev):
+    replace(getAppDir(), "/nimhapkg/mainmodules", "") / "/nimblecache"
+  else:
+    #installpath
+    "/var/run/nimha/nimblecache"
 
